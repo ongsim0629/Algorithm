60ee055a90a899054be0c544a05fc8ccc9cc58e7
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2023-03-13 15:44:11 +0900

    Update 두 수의 나눗셈.js
    코드 리뷰 업데이트

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -9,3 +9,20 @@ function solution(num1, num2) {
         console.log("wrong input!");
     }
 }
+
+/*
+다른 사람의 코드
+별의 별 함수가 다 있다 ㄷㄷ...
+물결은 진심 뭐임??..
+
+const solution = (num1, num2) => Math.floor(num1 / num2 * 1000)
+
+function solution(num1, num2) {
+    return Math.trunc(num1 / num2 * 1000);
+}
+
+function solution(num1, num2) {
+    return ~~(num1/num2*1000);
+}
+
+*/
