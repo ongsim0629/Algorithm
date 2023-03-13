eb391ba56179f753aa3c7ca3518a5930dc2a9792
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2023-03-13 15:17:51 +0900

    Update 숫자 비교하기.js

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -11,3 +11,12 @@ function solution(num1, num2) {
         console.log("wrong input!");
     }
 }
+
+/*
+신기했던 코드 -> 삼항 연산자 쓰니까 이게 이렇게 쉽네;;;
+
+function solution(num1, num2) {
+    var answer = num1 === num2 ? 1 : -1;
+    return answer;
+}
+*/
