a1330a5227c48623c0637d04bbe1e9a1523c32f2
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2023-03-16 15:09:48 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,26 @@
+function solution(angle) {
+    // 제안사항을 충족하는 경우
+    if ( 0 < angle && angle < 181 && Number.isInteger(angle) == true ){
+        // 예각
+        if (0 < angle && angle < 90) {
+            var answer = 1;
+        }
+        // 직각
+        else if(angle == 90) {
+            var answer = 2;
+        }
+        // 둔각
+        else if (90 < angle && angle < 180){
+            var answer = 3;
+        }
+        // 평각
+        else {
+            var answer = 4;
+        }
+        return answer;
+    }
+    // 제안사항을 만족하지 못 할 때
+    else {
+        console.log("wrong input!");
+    }
+}
