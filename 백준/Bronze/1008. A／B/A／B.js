fb2c080e61bf03ac23d73adc04c6a357b4348397
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2024-07-07 23:06:00 +0900

    [Bronze V] Title: A/B, Time: 100 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,10 @@
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "알고리즘/example.txt").toString().trim().split(" ").map(Number);
+
+function divide(arr){
+    let a = arr[0];
+    let b = arr[1];
+    return a/b;
+}
+
+let answer = divide(input)
+console.log(answer);
