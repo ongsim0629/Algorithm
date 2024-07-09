c534cfd53924a4b24571920e1611dc71266464bd
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2024-07-09 17:43:02 +0900

    병합 정렬 주석 추가

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.js
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.js
@@ -1,20 +1,15 @@
-const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "알고리즘/example.txt").toString().trim().split("\n").map((el) => el.split(" ").map(Number));
-const N = input[0][0];
-const K = input[0][1];
-let count = 0;
-let found = false;
-
-let A = [...input[1]];
-
 function merge_sort(A) {
+    // 재귀가 계속되면서 배열이 쪼개지다가 길이가 1이 되는 순간 -> 길이가 1이면 이미 정렬된 상태라고 보고 그냥 원소를 리턴해준다.
     if (A.length <= 1) {
         return A;
     }
     let middle = parseInt((A.length+1) / 2);
     let left_A = A.slice(0, middle);
     let right_A = A.slice(middle);
+    // 재귀에 의해서 계~속 쪼개지다가 원소 길이 1일 때의 값들이 각각 left와 right에 저장된다. (맨 앞의 두 값)
     left_A = merge_sort(left_A);
     right_A = merge_sort(right_A);
+    // 그러면 맨 앞의 두 값이 정렬된 배열 (즉 아래의 tmp리스트)가 left_A로 저장된다. 계속 반복하면 이제 아예 정렬되게된다.
     return merge(left_A, right_A);
 }
 
