be621c46d60b6500826142bd8743000656589a00
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2024-07-22 21:48:34 +0900

    체스판 다시 칠하기 주석 추가

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
@@ -10,14 +10,18 @@ const chess_1 = [ 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'B
 // 모범 체스판 2
 const chess_2 = [ 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW',  'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB' ]
 
+// 
 function check_board(arr){
     let count_list = [];
+    // 모범 체스판 1에 대해서 테스트 케이스와 비교
     for(let i = 0; i + 7 <= M; i++){
+        // 보드를 8*8짜리의 타겟 행렬로 자른다. (아래로 이동한 후 -> 오른쪽으로 이동)
         for(let j = 0; j + 7 < N; j++){
             let target_arr = [];
             for(let k = 0; k < 8; k++){
                 target_arr.push(test_case[Number(j+k)][0].slice(i,i+8));
             }
+            // 타겟 행렬과 모범 체스판 1을 하나하나 비교한다 -> 모범 체스판 1과 다르면 count +1
             let count = 0;
             for(let l = 0; l < 8; l++){
                 for(let m = 0; m < 8; m++){
@@ -26,15 +30,19 @@ function check_board(arr){
                     }
                 }
             }
+            // 모범 체스판 1과 타겟 행렬 사이의 비교가 끝나면 카운트 리스트에 해당 타겟 행렬의 카운트 삽입
             count_list.push(count);
         }
     }
+    // 모범 체스판 2에 대해서 테스트 케이스와 비교
     for(let i = 0; i + 7 <= M; i++){
+        // 보드를 8*8짜리의 타겟 행렬로 자른다. (아래로 이동한 후 -> 오른쪽으로 이동)
         for(let j = 0; j + 7 < N; j++){
             let target_arr = [];
             for(let k = 0; k < 8; k++){
                 target_arr.push(test_case[Number(j+k)][0].slice(i,i+8));
             }
+            // 타겟 행렬과 모범 체스판 2를 하나하나 비교한다 -> 모범 체스판 2와 다르면 count +1
             let count = 0;
             for(let l = 0; l < 8; l++){
                 for(let m = 0; m < 8; m++){
@@ -43,9 +51,11 @@ function check_board(arr){
                     }
                 }
             }
+            // 모범 체스판 2와 타겟 행렬 사이의 비교가 끝나면 카운트 리스트에 해당 타겟 행렬의 카운트 삽입
             count_list.push(count);
         }
     }
+    // 카운트 리스트의 최소값이 정답이다.
     let answer = Math.min.apply(null, count_list);
     return answer;
 }
