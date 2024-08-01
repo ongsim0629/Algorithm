d4d7934a0ce5325f9d8e1812cb4c950c62746d66
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2024-08-02 00:02:34 +0900

    [Silver II] Title: N과 M (11), Time: 496 ms, Memory: 64528 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.js
@@ -0,0 +1,29 @@
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "알고리즘/example.txt").toString().trim().split("\n").map((el) => el.split(" ").map(Number));
+const N = input[0][0];
+const M = input[0][1];
+const num_list = input[1].sort((a, b) => a - b);
+
+let state = new Array(M).fill(0);
+let answer = "";
+
+function backtracking(k) {
+
+    if (k == M) {
+        answer += state.join(" ") + "\n";
+        return;
+    }
+
+    else {
+        let lastUsed = -1;
+        for (let i = 0; i < N; i++) {
+            if (num_list[i] !== lastUsed) {
+                state[k] = num_list[i];
+                lastUsed = num_list[i];  // 현재 숫자를 lastUsed에 기록
+                backtracking(k + 1);
+            }
+        }
+    }
+}
+
+backtracking(0);
+console.log(answer);
