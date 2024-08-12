cc19cd3c19234eb70ec477b1dc2f07c3068a5d8b
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2024-08-12 19:05:45 +0900

    [Gold V] Title: 두 용액, Time: 124 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -3,18 +3,21 @@ input = sys.stdin.readline
 
 N = int(input())
 liquid_list = list(map(int, input().split(" ")))
+# 투 포인터 사용을 위한 정렬 
 liquid_list.sort()
 
 diff_max = float('inf')
 
+# 포인터 시작 위치 지정
 left = 0
 right = len(liquid_list) - 1
+
 answers = (liquid_list[left], liquid_list[right])
     
-# 투 포인터를 사용해야한다.
+# 투 포인터를 사용하면 시간 복잡도가 감소하게 된다.
 while left < right:
     current_sum = liquid_list[left] + liquid_list[right]
-    
+    # 0에 가깝다 -> 절댓값의 크기가 작다
     if abs(current_sum) < abs(diff_max):
         diff_max = current_sum
         answers = (liquid_list[left], liquid_list[right])
