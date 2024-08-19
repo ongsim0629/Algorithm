99c891e410101bdb89536c519a7a32a67f81fd45
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2024-08-19 19:42:31 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,3 +1,7 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
 N = int(input())
 num_list = list(map(int, input().split()))
 operator_num_list = list(map(int, input().split()))
@@ -8,6 +12,7 @@ min_result = float('inf')
 def dfs(depth, current_sum, plus, minus, multiply, divide):
     global max_result, min_result
     
+    # 연산자 다 쓰면 => 최대 최소 갱신해주기
     if depth == N:
         max_result = max(max_result, current_sum)
         min_result = min(min_result, current_sum)
@@ -20,10 +25,8 @@ def dfs(depth, current_sum, plus, minus, multiply, divide):
     if multiply > 0:
         dfs(depth + 1, current_sum * num_list[depth], plus, minus, multiply - 1, divide)
     if divide > 0:
-        # Python에서 음수 나눗셈은 주의가 필요하므로 int()를 사용하여 정수 나눗셈을 처리
         dfs(depth + 1, int(current_sum / num_list[depth]), plus, minus, multiply, divide - 1)
 
-# 초기 상태에서 DFS 시작
 dfs(1, num_list[0], operator_num_list[0], operator_num_list[1], operator_num_list[2], operator_num_list[3])
 
 print(max_result)
