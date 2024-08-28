7c13336034de3bda1afdd1fa24f7b0e57b213ff9
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2024-08-28 14:08:22 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -6,17 +6,15 @@ T = int(input())
 for _ in range(T):
     k = int(input())
     n = int(input())
-    dp = [[0] * (n + 1) for _ in range(k+1)]
+
+    dp = [[0] * (n + 1) for _ in range(k + 1)]
     
-    # 0층 주민 배치
-    for i in range(n + 1):
+    for i in range(1, n + 1): 
         dp[0][i] = i
     
-    for floor in range(1, k+1):
-        for ho in range(n+1):
-            hap = 0
-            for count in range(ho+1):
-                hap += dp[floor-1][count]
-            dp[floor][ho] = hap
-            
-    print(dp[k][n])
+    # DP 테이블 채우기
+    for i in range(1, k + 1): 
+        for j in range(1, n + 1): 
+            dp[i][j] = dp[i][j-1] + dp[i-1][j]  
+    
+    print(dp[k][n]) 
