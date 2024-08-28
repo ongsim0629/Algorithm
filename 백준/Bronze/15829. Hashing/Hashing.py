1b56c254ca419cdeb7dc88aadd1d986e6a2e7c83
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2024-08-28 14:20:37 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,7 +2,13 @@ L = int(input())
 string = input()
 
 hap = 0
-for idx, char in enumerate(string):
-    num = ord(char) - 96
-    hap += num * 31**idx
+mod = 1234567891  # 문제에서 주어진 소수
+
+power_of_31 = 1  # 31의 거듭제곱을 저장할 변수
+
+for i in range(L):
+    num = ord(string[i]) - 96  # 'a' -> 1, 'b' -> 2, ..., 'z' -> 26
+    hap = (hap + num * power_of_31) % mod  # 모듈로 연산을 사용하여 큰 수를 방지
+    power_of_31 = (power_of_31 * 31) % mod  # 다음 거듭제곱을 위해 31을 곱함
+
 print(hap)
