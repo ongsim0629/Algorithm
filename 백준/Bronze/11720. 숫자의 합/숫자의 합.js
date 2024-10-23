8a96a71291b343740a2b553482ca62b28306cb73
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2024-10-23 15:13:18 +0900

    [Bronze IV] Title: 숫자의 합, Time: 100 ms, Memory: 9576 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.js
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -1,10 +1,12 @@
-const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "알고리즘/example.txt").toString().trim().split("\n");
-const N = input[0];
-const numbers = input[1].split("").map(Number);
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "example.txt").toString().trim().split("\n").map((el) => el.split(" "));
 
-let answer = 0;
-for(let i = 0; i < N; i++){
-    answer += numbers[i];
+let N = input[0][0];
+let number = input[1][0];
+
+let arr = number.toString().split('').map(Number);
+
+let sum = 0;
+for (let i = 0; i < N; i++){
+    sum += arr[i]
 }
-
-console.log(answer);
+console.log(sum);
