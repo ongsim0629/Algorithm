fed95ae3d29966b32374c4857a47a3da1d07d9e5
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2025-01-10 21:51:32 +0900

    Time: 0 ms (100%), Space: 48.6 MB (80.41%) - LeetHub

---
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    dp = [];
+    dp[0]=0;
+    dp[1]=1;
+    if(n > 1){
+        dp[2] = 2;
+        if(n==2){
+            return 2;
+        }
+    }
+    else{
+        return 1;
+    }
+    
+    for(let i = 3; i < n+1; i++){
+        dp[i] = dp[i-1]+dp[i-2];
+    }
+    
+    return dp[n];
+    
+};
