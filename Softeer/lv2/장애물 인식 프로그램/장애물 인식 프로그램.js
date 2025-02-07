c5d3fe794d7a3bcc62fab05ce9ba9b71bf156d80
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2025-02-07 18:28:29 +0900

    Update 장애물 인식 프로그램.js

--- Softeer/lv2/장애물 인식 프로그램/장애물 인식 프로그램.js
+++ Softeer/lv2/장애물 인식 프로그램/장애물 인식 프로그램.js
@@ -1,44 +1,40 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin','utf8').trim().split('\n');
+const input = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+
 const N = Number(input[0]);
 const map = input.slice(1,N+1).map(x=>x.split('').map(Number));
-let visited = Array(N).fill().map(()=>Array(N).fill(0));
-const dx = [0, 0, 1, -1];
-const dy = [1, -1, 0, 0];
+let visited = Array(N).fill().map(()=>Array(N).fill(false));
 
-let count = 0;
-let ans = [];
+const dx = [0,0,1,-1];
+const dy = [1,-1,0,0];
 
-function dfs(x, y) {
-    let cnt = 1;  // 현재 단지의 집의 수
-    visited[x][y] = 1;
-    
-    // 4방향 탐색
-    for(let k = 0; k < 4; k++) {
-        let nx = x + dx[k];
-        let ny = y + dy[k];
-        
-        // 범위 체크 및 방문하지 않은 집 체크
-        if(nx >= 0 && nx < N && ny >= 0 && ny < N && 
-           map[nx][ny] === 1 && visited[nx][ny] === 0) {
-            cnt += dfs(nx, ny);  // 연결된 집의 수를 더함
+let answer = [];
+
+// 재귀 함수 -> 장애물에서 상하좌우 탐색하며 주변에 장애물 없을 때 까지
+function dfs(x,y){
+    let obstacle = 1;
+    visited[x][y] = true;
+
+    for (let i = 0; i < 4; i++){
+        const new_x = x+dx[i];
+        const new_y = y+dy[i];
+        if(new_x >= 0 && new_x < N && new_y >= 0 && new_y < N && map[new_x][new_y] === 1 && visited[new_x][new_y] === false){
+            visited[new_x][new_y] = true;
+            obstacle += dfs(new_x,new_y);
         }
     }
-    
-    return cnt;
+    return obstacle;
 }
 
-for(let i = 0; i < N; i++) {
-    for(let j = 0; j < N; j++) {
-        if(map[i][j] === 1 && visited[i][j] === 0) {
-            ans.push(dfs(i, j));
-            count++;
+for (let i = 0; i < N; i++){
+    for (let j = 0; j < N; j++){
+        if(map[i][j] === 1 && visited[i][j] === false){
+            answer.push(dfs(i,j));
         }
     }
 }
 
-console.log(count);
-const answer = ans.sort((a,b)=>a-b);
-for (let i = 0; i<answer.length; i++){
-    console.log(answer[i]);
+console.log(answer.length);
+for (ans of answer){
+    console.log(ans);
 }
